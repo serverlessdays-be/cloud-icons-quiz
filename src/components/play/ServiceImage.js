a031af346ba8f6cd5996de781c5fac1b051c499c
Nxtra <nickvanhoof1@gmail.com>
2020-12-02 23:55:06 +0100

    Display loading image when image is loading

--- src/components/play/ServiceImage.js
+++ src/components/play/ServiceImage.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 import { Storage } from "aws-amplify";
+import LoadingSpinner from "../LoadingSpinner";
 
 const ServiceImage = ({ s3Key }) => {
+  const [loading, setLoading] = useState(true);
   const [imageSignedUrl, setImage] = useState();
 
   useEffect(() => {
+    const selectFile = async () => {
+      const signedUrl = await Storage.get(s3Key, { level: "public" });
+      setImage(signedUrl);
+      setLoading(false);
+    };
+
     if (s3Key) {
       selectFile();
     }
   }, [s3Key]);
 
-  const selectFile = async () => {
-    const signedUrl = await Storage.get(s3Key, { level: "public" });
-    setImage(signedUrl);
-  };
+  if (loading) return <LoadingSpinner />;
 
   return (
     <div className="d-flex justify-content-center">
